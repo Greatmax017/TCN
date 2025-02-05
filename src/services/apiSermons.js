63283d440f4e2d1197a17bfb107e45319caeef56
maxwell <techgbenga@gmail.com>
2025-02-05 03:18:53 +0100

    added method

--- src/services/apiSermons.js
+++ src/services/apiSermons.js
@@ -1,7 +1,13 @@
 const API_URL = import.meta.env.VITE_API_BASE_URL
 
 export const getPodcastEpisodes = async (showId) => {
-    const response = await fetch(`${API_URL}/api/episodes/${showId}`);
+    const response = await fetch(`${API_URL}/api/episodes/${showId}`,{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+    });
     if (!response.ok) {
       throw new Error('Failed to fetch episodes');
     }
